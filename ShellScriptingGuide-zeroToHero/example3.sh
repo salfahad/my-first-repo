65a6336153887621fe760ec0e7f55b8ff7341fc7
EC2 Default User <ec2-user@ip-172-31-60-84.ec2.internal>
2024-07-25 05:50:25 +0000

    testing

---
+++ ShellScriptingGuide-zeroToHero/example3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 7 ] ; then
+echo "First argument: $1"
+echo "second argument: $2"
+echo "second argument: $3"
+echo "second argument: $4"
+echo "second argument: $7"
+else
+echo "Provide excetly two arguments"
+
+fi
